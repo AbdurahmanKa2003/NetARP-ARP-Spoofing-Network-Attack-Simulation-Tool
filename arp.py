b71eafe5d9d082dcf6d2bfc3b91d8c0cb0f7dbfb
AbdurahmanKa2003 <147339917+AbdurahmanKa2003@users.noreply.github.com>
2026-02-22 08:54:19 +0300

    Update arp.py

--- arp.py
+++ arp.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python3
-
 from scapy.all import sniff, ARP, Ether
 from datetime import datetime
 
-# Наша "чистая" ARP-таблица
+
 ip_mac_map = {}
 
 def process_packet(packet):
-    # Нас интересуют только ARP-пакеты
+    
     if packet.haslayer(ARP) and packet.haslayer(Ether):
 
         src_ip = packet[ARP].psrc
         src_mac = packet[Ether].src
 
-        # Если MAC уже есть в таблице
+       
         if src_mac in ip_mac_map:
             if ip_mac_map[src_mac] != src_ip:
                 print("\n[!] POSSIBLE ARP SPOOFING DETECTED!")
@@ -23,10 +21,10 @@ def process_packet(packet):
                 print(f"    New IP: {src_ip}")
                 print("    This MAC is pretending to be another IP!\n")
         else:
-            # Запоминаем нормальное соответствие
+         
             ip_mac_map[src_mac] = src_ip
 
-# Запуск сниффинга
+
 print("[*] ARP detector started...")
 print("[*] Listening for ARP packets...\n")
 
